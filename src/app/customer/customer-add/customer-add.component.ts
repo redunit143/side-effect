784d1c0bf2dff5bc7a7ff7eeb6956ebf9febcf74
WiL <will@machinecompare.com>
2022-03-16 08:10:57 +0000

    bugfix for add component. Missing code added. plus removed extraneous characters from html file

--- src/app/customer/customer-add/customer-add.component.ts
+++ src/app/customer/customer-add/customer-add.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../models/customer';
+import { select, Store } from '@ngrx/store';
+import { CustomerState } from '../store/reducer/customer.reducer';
+import * as CustomerActions from '../store/action/customer.actions';
+
 
 @Component({
   selector: 'app-customer-add',
   templateUrl: './customer-add.component.html',
   styleUrls: ['./customer-add.component.scss']
 })
+
 export class CustomerAddComponent {
 
   constructor(private store: Store<CustomerState>) {
@@ -13,7 +19,6 @@ export class CustomerAddComponent {
   addCustomer(customerName: string): void {
     const customer = new Customer();
     customer.name = customerName;
-    this.store.dispatch(addCustomer(customer));
+    this.store.dispatch(CustomerActions.addCustomer(customer));
   }
-
 }
