7ece69d3ab68a6a7bee369119c6438dc2f0a0c2e
WiL <will@machinecompare.com>
2022-03-16 08:15:10 +0000

    bugfix for customer actions. Bad instructions. prefix 'load' was used, 'add' was required by th add component

--- src/app/customer/store/action/customer.actions.ts
+++ src/app/customer/store/action/customer.actions.ts
@@ -1,11 +1,14 @@
 import { createAction, props } from '@ngrx/store';
 import {Customer} from '../../../models/customer';
 
+// Creatd by the generator and not used
 export const loadCustomers = createAction(
   '[Customer] Load Customers',
   (customer: Customer) => ({customer})
 );
 
 
-
-
+export const addCustomer = createAction(
+  '[Customer] Add Customer',
+  (customer: Customer) => ({customer})
+);
